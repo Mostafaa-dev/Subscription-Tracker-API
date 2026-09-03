627cc04b3192dcd2262d2c9c5893b83cc41d0dcf
Mostafa Fayad <mostafa.socialx@gmail.com>
2026-09-03 08:04:49 +0300

    feat: add MongoDB database connection

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 import express from "express";
 
 import { PORT } from "./config/env.js";
-
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRoutes from "./routes/subscription.routes.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRoutes);
 
+// Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(PORT, () => {
-  console.log(
-    `Subscription Tracker API is running on http://localhost:${PORT}`,
-  );
-});
+// Start server
+const startServer = async () => {
+  try {
+    await connectToDatabase();
+
+    app.listen(PORT, () => {
+      console.log(
+        `Subscription Tracker API is running on http://localhost:${PORT}`,
+      );
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
 
 export default app;
